8bcc4cd9a05e2fa4b2bcbe5706f29113f25c03ae
Bill Vittis <vvittis@umass.edu>
2025-03-24 20:30:22 -0400

    Initial Commit

---
+++ FocusExtention/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FocusExtention
+//
+//  Created by Vasilis Vittis on 3/24/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ FocusExtentionTests/FocusExtentionTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusExtentionTests.swift
+//  FocusExtentionTests
+//
+//  Created by Vasilis Vittis on 3/24/25.
+//
+
+import Testing
+@testable import FocusExtention
+
+struct FocusExtentionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
